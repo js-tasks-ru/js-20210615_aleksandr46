4f562146b0bb9918450b29e0061086b5c3110848
Alexandr <k.v.alexandr@gmail.com>
2021-06-24 11:47:27 +0400

    add: task 3

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,14 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
+  return function (obj) {
+    const arrPath = path.split(".");
+    let result;
 
+    for (let i = 0; i < arrPath.length; i++) {
+      result =
+        typeof result === "object" ? result[arrPath[i]] : obj[arrPath[i]];
+    }
+    return result;
+  };
 }
